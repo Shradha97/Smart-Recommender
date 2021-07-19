720036cbc84dab184d06bd1842554606f4224dde
S@i <me16b021@iittp.ac.in>
2021-07-19 20:11:11 +0530

    Add files via upload

--- Mindbody.Vista.SmartRecommender/app/engine/time_slot_insight_engine.py
+++ Mindbody.Vista.SmartRecommender/app/engine/time_slot_insight_engine.py
@@ -4,50 +4,53 @@ import pandas as pd
 
 class TimeSlotInsightEngine:
     def time_slot_insights(self, booking_trends_df, search_trends_df, category):
-        #for category in category_list:
+        # for category in category_list:
         ranked_time_slot_insights_df = self.get_area_between_trends(booking_trends_df, search_trends_df, category)
         return ranked_time_slot_insights_df
 
     def get_area_between_trends(self, booking_trends_df, search_trends_df, category):
         demand_supply_gap_approx = []
+        categories = search_trends_df['Category'].unique()
+        print(categories)
 
-        for index, row in search_trends_df.iterrows():
-            time_slot_start = row['Time_Slot_Start']
-            time_slot_end = row['Time_Slot_End']
+        for category in categories:
+            for index, row in search_trends_df.iterrows():
+                time_slot_start = row['Time_Slot_Start']
+                time_slot_end = row['Time_Slot_End']
 
-            booking_trend_row = booking_trends_df.loc[(booking_trends_df['Time_Slot_Start'].isin([time_slot_start,
-                                                                                                  time_slot_end]))]
-            search_trend_row = search_trends_df.loc[(search_trends_df['Time_Slot_Start'].isin([time_slot_start,
-                                                                                               time_slot_end]))]
+                booking_trend_row = booking_trends_df.loc[(booking_trends_df['Time_Slot_Start'].isin([time_slot_start,
+                                                                                                      time_slot_end]))]
+                search_trend_row = search_trends_df.loc[(search_trends_df['Time_Slot_Start'].isin([time_slot_start,
+                                                                                                   time_slot_end]))]
 
-            if booking_trend_row.empty:
-                booking_aggregate_pair = [0, 0]
-            elif len(booking_trend_row) == 1:
-                booking_aggregate_pair = [booking_trend_row['Bookings_Aggregate'].iloc[0], 0]
-            else:
-                booking_aggregate_pair = [booking_trend_row['Bookings_Aggregate'].iloc[0],
-                                          booking_trend_row['Bookings_Aggregate'].iloc[1]]
+                if booking_trend_row.empty:
+                    booking_aggregate_pair = [0, 0]
+                elif len(booking_trend_row) == 1:
+                    booking_aggregate_pair = [booking_trend_row['Bookings_Aggregate'].iloc[0], 0]
+                else:
+                    booking_aggregate_pair = [booking_trend_row['Bookings_Aggregate'].iloc[0],
+                                              booking_trend_row['Bookings_Aggregate'].iloc[1]]
 
-            if search_trend_row.empty:
-                search_aggregate_pair = [0, 0]
-            elif len(search_trend_row) == 1:
-                search_aggregate_pair = [search_trend_row['Search_Aggregate'].iloc[0], 0]
-            else:
-                search_aggregate_pair = [search_trend_row['Search_Aggregate'].iloc[0],
-                                         search_trend_row['Search_Aggregate'].iloc[1]]
+                if search_trend_row.empty:
+                    search_aggregate_pair = [0, 0]
+                elif len(search_trend_row) == 1:
+                    search_aggregate_pair = [search_trend_row['Search_Aggregate'].iloc[0], 0]
+                else:
+                    search_aggregate_pair = [search_trend_row['Search_Aggregate'].iloc[0],
+                                             search_trend_row['Search_Aggregate'].iloc[1]]
 
-            demand_supply_gap_area = abs(trapz(search_aggregate_pair, dx=1) -
-                                         trapz(booking_aggregate_pair, dx=1))
+                demand_supply_gap_area = abs(trapz(search_aggregate_pair, dx=1) -
+                                             trapz(booking_aggregate_pair, dx=1))
 
-            demand_supply_gap_approx.append(
-                (
-                    category,
-                    search_trend_row['Search_Aggregate'].iloc[0],
-                    time_slot_start,
-                    time_slot_end,
-                    demand_supply_gap_area,
+                demand_supply_gap_approx.append(
+                    (
+                        category,
+                        search_trend_row['Search_Aggregate'].iloc[0],
+                        time_slot_start,
+                        time_slot_end,
+                        demand_supply_gap_area,
+                    )
                 )
-            )
 
         demand_supply_gap_approx_df = self._list_to_df(demand_supply_gap_approx)
         ranked_recommended_time_slots = self.rerank_time_slots(demand_supply_gap_approx_df)
@@ -61,7 +64,7 @@ class TimeSlotInsightEngine:
 
         # dropping unnecessary columns
         ranked_by_aggregate_and_area = ranked_by_aggregate_and_area.drop(
-            ['Search_Aggregate', 'Demand_Supply_Gap_Approx']
+            ['Demand_Supply_Gap_Approx']
             , axis=1)
         return ranked_by_aggregate_and_area
 
--- Mindbody.Vista.SmartRecommender/app/engine/trends_engine.py
+++ Mindbody.Vista.SmartRecommender/app/engine/trends_engine.py
@@ -1,23 +1,27 @@
 from app.settings import BOOKINGS_DATA_PATH, SEARCH_LOG_DATA_PATH
 import pandas as pd
+import itertools
+
+studio_categories = []
 
 
 class TrendsEngine:
-    def get_hourly_booking_trends(self, category):
-        booking_df = self._get_booking_data(category)
-        booking_trend_df = self._calculate_hourly_aggregates(booking_df, category, 'Classtime')
+    def get_hourly_booking_trends(self, studio_id, category):
+        booking_df = self._get_booking_data(studio_id, category)
+        booking_trend_df = self._calculate_hourly_aggregates(booking_df, 'PARENTCATEGORY', 'Classtime')
         return booking_trend_df
 
     def get_hourly_search_log_trends(self, category):
         search_log_df = self._get_search_log_data(category)
-        search_log_trend_df = self._calculate_hourly_aggregates(search_log_df, category, 'SEARCHTIME')
+        search_log_trend_df = self._calculate_hourly_aggregates(search_log_df, 'CATEGORY', 'SEARCHTIME')
         return search_log_trend_df
 
-    def _calculate_hourly_aggregates(self, df, category, column_name):
+    def _calculate_hourly_aggregates(self, df, category_col, column_name):
         trends_list = []
         df[column_name] = pd.to_datetime(df[column_name])
+        print()
 
-        for start_hour in range(5, 21):
+        for start_hour, category in itertools.product(range(5, 21), df[category_col].unique()):
             aggregate_at_time_slot_df = df.loc[
                 (df[column_name].dt.hour >= int(start_hour))
                 & (df[column_name].dt.hour < int(start_hour) + 1)
@@ -34,15 +38,23 @@ class TrendsEngine:
                     )
                 )
         return self._list_to_df(trends_list, column_name)
-    
-    @staticmethod
-    def _get_booking_data(category):
+
+    def _get_booking_data(self, studio_id, category):
+        global studio_categories
         df = pd.read_csv(BOOKINGS_DATA_PATH)
+        studio_categories = self._get_categories_offered_by_studio(df, studio_id, 'PARENTCATEGORY')
+        if not category:
+            common_category_data = self._get_filtered_category_data(df, 'PARENTCATEGORY')
+            most_popular_category_data = self._get_most_popular_category_data(common_category_data, 'PARENTCATEGORY')
+            return most_popular_category_data
         return df.loc[df['PARENTCATEGORY'] == category]
 
-    @staticmethod
-    def _get_search_log_data(category):
+    def _get_search_log_data(self, category):
         df = pd.read_csv(SEARCH_LOG_DATA_PATH)
+        if not category:
+            common_category_data = self._get_filtered_category_data(df, 'CATEGORY')
+            most_popular_category_data = self._get_most_popular_category_data(common_category_data, 'CATEGORY')
+            return most_popular_category_data
         return df.loc[df['CATEGORY'] == category]
 
     @staticmethod
@@ -55,3 +67,29 @@ class TrendsEngine:
         df = pd.DataFrame(trends_list, columns=['Category', 'Time_Slot_Start', 'Time_Slot_End', time_type,
                                                 aggregate_type])
         return df
+
+    @staticmethod
+    def _get_filtered_category_data(data, column_name):
+        global studio_categories
+        top_categories = data[column_name].unique()
+        common_categories = list(set(top_categories) & set(studio_categories))
+        filtered_data = data.loc[data[column_name].isin(common_categories)]
+        return filtered_data
+
+    @staticmethod
+    def _get_categories_offered_by_studio(data, studio_id, column_name):
+        categories = data.loc[data['StudioId'] == int(studio_id)][column_name].unique()
+        return categories
+
+    @staticmethod
+    def _get_common_categories(top_categories):
+        global studio_categories
+        common_categories = list(set(top_categories) & set(studio_categories))
+        return common_categories
+
+    @staticmethod
+    def _get_most_popular_category_data(data, column_name, n=3):
+        n = max(n, len(data[column_name].unique()))
+        top_categories = data[column_name].value_counts()[:n].index.tolist()
+        filtered_data = data.loc[data[column_name].isin(top_categories)]
+        return filtered_data
